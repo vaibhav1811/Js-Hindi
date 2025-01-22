8c1f8d43955411b0f730d05804d99a97e8109f92
vaibhav1811 <121346840+vaibhav1811@users.noreply.github.com>
2025-01-22 05:08:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //it will treat all js code as newer version
+
+//alert(3+3)// wont work as we are using nodejs, not browser
+console.log("Vaibhav")
+
+
+let name = "vaibhav"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // it shows null as object
