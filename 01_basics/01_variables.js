2a3363e20560056be054f09db738e5ca89c14f36
vaibhav1811 <121346840+vaibhav1811@users.noreply.github.com>
2025-01-22 05:10:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountid = 198754
+let accountEmail= "Vaibhav@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountid= 2// not allowed
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+
+*/
+
+accountEmail ="vsvs@gcom"
+accountPassword="9876"
+accountCity="Bengaluru"
+
+console.table([accountEmail,accountPassword,accountid,accountCity,accountState])
